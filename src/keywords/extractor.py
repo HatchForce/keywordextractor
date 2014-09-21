7d6591e1edb16e573218a8514ec03044e59bffff
Christian Kissig <christian.kissig@oracle.com>
2014-09-21 12:18:28 +0200

    Sentence splitting

--- src/keywords/extractor.py
+++ src/keywords/extractor.py
@@ -9,36 +9,38 @@ filter.loadPositiveList("../data/keywords/positives.txt")
 filter.loadNegativeList("../data/keywords/negatives.txt")
 casing.loadCasings("../data/keywords/casings.txt")
 
-from util.tokenizer import separatewords
+from util.tokenizer import separatewords, splitSentences
 from rake import calculateWordScores, generateCandidateKeywordScores
 from stopwords import splitByStopWords
 def extract(text):
     # Extracts keywords from text
     
     # preprocess, tokenize, group in n-grams
-    text = util.textprocess.preprocess(text)     
-    tokens = separatewords(text, 0)
-    phrases = splitByStopWords(tokens)
-    phrases = [ngram for phrase in phrases for n in range(3) for ngram in generate_ngrams(phrase, n + 1)]
-    
-    print phrases
+    text = util.textprocess.preprocess(text)
+    sentences = [separatewords(sentence, 0) for sentence in splitSentences(text)]
+    phrases = [phrase for sentence in sentences for phrase in splitByStopWords(sentence)]    
+    phrases = [ngram for phrase in phrases for n in range(3) for ngram in generate_ngrams(phrase, n + 1)]    
     
     # filter
     filtered = [' '.join(ngram) for ngram in filter.filter(phrases)]
-    print filtered
     
     # casings
     casings = {}
     for phrase in phrases:
         casings[' '.join(phrase)] = casing.normalize(phrase)
-    print casings    
     
     # RAKE
     wordscores = calculateWordScores(phrases)        
     keywords = generateCandidateKeywordScores(phrases, wordscores)
-    keywords = sorted(keywords.iteritems(), key=operator.itemgetter(1), reverse=True)
+    
+    # Normalize scores
+    maxWeight = max(keywords.values())
+    if maxWeight > 0:
+        for keyphrase in keywords.keys():
+            keywords[keyphrase] /= maxWeight
     
     # format    
+    keywords = sorted(keywords.iteritems(), key=operator.itemgetter(1), reverse=True)
     return [{'keyword':casings.setdefault(pair[0], pair[0]), 'weight':pair[1]} for pair in keywords if pair[0] in filtered]
 
 def generate_ngrams(tokens, n):
--- src/util/tokenizer.py
+++ src/util/tokenizer.py
@@ -1,11 +1,13 @@
 import re
 from util import isnum
 
+splitter = re.compile('[^a-zA-Z0-9_\\+\\-/]')
+sentenceDelimiters = re.compile(u'[.!?,;:\t\\-\\"\\(\\)\\\'\u2019\u2013]')
+
 # Utility function to return a list of all words that are have a length greater than a specified number of characters.
 # @param text The text that must be split in to words.
 # @param minWordReturnSize The minimum no of characters a word must have to be included.
-def separatewords(text, minWordReturnSize):
-    splitter = re.compile('[^a-zA-Z0-9_\\+\\-/]')
+def separatewords(text, minWordReturnSize):    
     words = []
     for singleWord in splitter.split(text):
         currWord = singleWord.strip().lower()
@@ -16,7 +18,5 @@ def separatewords(text, minWordReturnSize):
 
 # Utility function to return a list of sentences.
 # @param text The text that must be split in to sentences.
-def splitSentences(text):
-    sentenceDelimiters = re.compile(u'[.!?,;:\t\\-\\"\\(\\)\\\'\u2019\u2013]')
-    sentenceList = sentenceDelimiters.split(text)
-    return sentenceList
+def splitSentences(text):    
+    return sentenceDelimiters.split(text)
